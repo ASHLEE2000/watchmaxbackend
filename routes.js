f257320b1b84e13f4b6f6b81ee1b17d59d49a0b6
ASH LEE <110445741+ASHLEE2000@users.noreply.github.com>
2024-01-22 21:35:48 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -8,6 +8,8 @@ const router = express.Router();
 const { authenticateToken } = require('./authMiddleware');
 const { videoController, getHealth, upload } = require('./controller');  // Import 'upload' from controller
 const { login, refreshToken, getProtectedData } = require('./authController');
+const path = require('path');
+const fs = require('fs');
 
 
 //protected routes
@@ -20,6 +22,28 @@ router.get('/health', getHealth);
 // routes to create user and handle authentication for the app
 router.post('/login', login);
 router.post('/refreshToken', refreshToken);
+
+app.delete('/delete', async (req, res) => {
+    const folderName = req.params.folderName;
+    const folderPath = path.join(__dirname, 'video', folderName); // Update 'your-base-folder' with the actual base folder path
+  
+    try {
+      // Check if the folder exists
+      const folderExists = await fs.access(folderPath, fs.constants.F_OK).then(() => true).catch(() => false);
+  
+      if (!folderExists) {
+        return res.status(404).json({ error: 'Folder not found' });
+      }
+  
+      // Delete the folder and its contents
+      await fs.rmdir(folderPath, { recursive: true });
+  
+      res.status(200).json({ message: 'Folder deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting folder:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 module.exports = router;
 
